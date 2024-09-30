1bebbf077680244305142288b6c3c35d6b431a3b
Jordan Ward <jordancward@gmail.com>
2024-09-30 08:35:26 -0600

    Add multi-line text from text files to PDF

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
-import pandas as pd
 import glob
 from fpdf import FPDF
 from pathlib import Path
-# import openpyxl, might need to install it?
 
-# openpyxl is a Python library to read/write Excel files
-# glob allows you to search for files with specific patterns
-
-# get all files ending with .xlsx
+# Get all files ending with .xlsx
 text_files = glob.glob("text_files/*.txt")
-
-# create only one pdf document because it's outside the for loop
+# Create one pdf document
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 for filepath in text_files:
 
     pdf.add_page()
 
-    # this pandas command only works with openpxyl downloaded
-    df = pd.read_csv(filepath)
-
-    # extracting the name of the file
+    # Extracting the name of the file
     filename = Path(filepath).stem
     animal_name = filename.title()
 
+    # Add the name to the PDF
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=animal_name, ln=1)
 
+    # Get the content of each text file
+    with open(filepath, "r") as file:
+        content = file.read()
 
-# create the pdf
+    # Add the text file content to the PDF
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=6, txt=content)
+
+
+
+# Produce the pdf
 pdf.output("output.pdf")
